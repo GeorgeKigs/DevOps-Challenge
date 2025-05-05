c2be4b9310d5f2ef1441de0bfa01506be56649f5
GeorgeKigs <gkndungu99@gmail.com>
2025-05-05 23:58:21 +0300

    Pushing ansible configurations for docker

--- DevOps/Scripts/scripts.sh
+++ DevOps/Scripts/scripts.sh
@@ -21,3 +21,29 @@ ansible-playbook -i ansible/inventory.ini ansible/playbook.yml -vvv
 
 # login to ecr
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 992122884453.dkr.ecr.eu-central-1.amazonaws.com
+
+
+# build and deploy backend.
+scp -P 1337 .env   ubuntu@3.76.209.15:/home/ubuntu/configs/.env #move the env file
+
+mvn clean install
+docker build -t validator-backend-image .
+docker tag validator-backend-image:latest 992122884453.dkr.ecr.eu-central-1.amazonaws.com/validator-backend-image:latest
+docker run -p 8080:8080 --env-file .env  992122884453.dkr.ecr.eu-central-1.amazonaws.com/validator-backend-image:latest
+
+
+
+# build frontend
+npm install
+npm run build
+docker build -t validator-frontend-image .
+
+
+docker tag validator-frontend-image:latest 992122884453.dkr.ecr.eu-central-1.amazonaws.com/validator-frontend-image:latest
+docker push 992122884453.dkr.ecr.eu-central-1.amazonaws.com/validator-frontend-image:latest
+docker run -p 8081:8081 992122884453.dkr.ecr.eu-central-1.amazonaws.com/validator-frontend-image:latest
+
+
+
+
+open http://localhost:8081
