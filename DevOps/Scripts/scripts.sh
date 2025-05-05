6795be4d988f3272c675fa485c90739073d27c28
GeorgeKigs <gkndungu99@gmail.com>
2025-05-05 23:10:43 +0300

    Configuring ansible scripts

--- DevOps/Scripts/scripts.sh
+++ DevOps/Scripts/scripts.sh
@@ -3,7 +3,7 @@ cd ~/.ssh
 ssh-keygen -t rsa -b 4096 -f terraform_key
 
 # transfer the key to the main node
-scp -i ~/.ssh/terraform_key ubuntu@172.31.149.4
+scp -P 1337 -i ~/.ssh/terraform_key ubuntu@172.31.149.4
 
 # install ansible on the main server
 sudo apt update
@@ -11,9 +11,13 @@ python3 --version
 sudo apt install python3-pip
 pip3 install ansible ansible-navigator
 
-
 # verify installation of packages
-
 ansible --version
 docker --version
-nginx
+nginx -v
+
+# run ansible
+ansible-playbook -i ansible/inventory.ini ansible/playbook.yml -vvv
+
+# login to ecr
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 992122884453.dkr.ecr.eu-central-1.amazonaws.com
